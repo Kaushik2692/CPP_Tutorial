bc3ba52e62561054da1db59b933171ae630e04fe
Kaushik2692 <kaushikkotian707@gmail.com>
2026-07-16 15:57:36 +0530

    Add compariso operator cpp file

---
+++ 01_CPP_Fundamentals/04_Operators/comparison_operator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    /**COMPARISON OPERATOR
+     * == (Equal to)
+     * != (Not equal to)
+     * <= (Less than or equal to)
+     * >= (Greater than or equal to)
+     * < (Less than)
+     * > (Greater than)
+     */
+
+    // Comparision operator retourn true or false
+
+    std::cout << (10 > 2);  // 1 => true
+    std::cout << (10 == 2); // 0 => false
+
+    return 0;
+}
