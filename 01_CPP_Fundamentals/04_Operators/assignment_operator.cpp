12017372cdc022e80e79bdd45cb1a7cad7b6bc63
Kaushik2692 <kaushikkotian707@gmail.com>
2026-07-16 15:43:34 +0530

    Add assignment operator cpp file

---
+++ 01_CPP_Fundamentals/04_Operators/assignment_operator.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    // = (Assignment Operator)
+    int age = 18; // Value 18 is assigned to variable age
+
+    // Chained assignment
+    int a, b, c;
+    a = b = c = 10;
+
+    /* Same as
+    c = 10;
+    b = c;
+    a = b;
+    */
+    return 0;
+}
